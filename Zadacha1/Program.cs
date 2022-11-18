e3426602d57f99790c5adc61fb9caef531837d4a
Анастасия <dn2126@bk.ru>
2022-11-18 22:45:28 +0300

    Add Zadacha1

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+
+
+Console.Write("Введите трёхзначное число: ");
+
+int threedigit_number = Convert.ToInt32(Console.ReadLine());
+
+string stringNumber = Convert.ToString(threedigit_number);
+
+Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
+
